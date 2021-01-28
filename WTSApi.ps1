502aad86627e839e8a243f43060642cd6fd58bf6
Guy Leech <guyrleech@gmail.com>
2021-01-28 15:11:30 +0000

    Added WTSClientInfo
    Fixed structures so not unsafe

--- WTSApi.ps1
+++ WTSApi.ps1
@@ -4,16 +4,25 @@
 
     Some code adapted from https://www.pinvoke.net/default.aspx/wtsapi32.wtsenumeratesessions
 
+    https://docs.microsoft.com/en-us/windows/win32/api/wtsapi32/nf-wtsapi32-wtsenumeratesessionsw
+
+    https://docs.microsoft.com/en-us/windows/win32/api/wtsapi32/nf-wtsapi32-wtsquerysessioninformationw
+
     @guyrleech 2018
 
     Modification History:
 
     21/01/19  GRL Changed Get-WTSSessionInformation to take array of computer names
+    28/01/19  GRL Added WTSClientInfo
+    30/01/20  GRL Fixed bug causing duplicate sessions
+    04/02/20  GRL Added session state field
+    27/01/21  GRL Added WTSClientInfo call to WTSQuerySessionInformation()
 #>
 
 Add-Type -ErrorAction Stop -TypeDefinition @'
     using System;
     using System.Runtime.InteropServices;
+
     public enum WTS_CONNECTSTATE_CLASS
     {
         WTSActive,
@@ -27,7 +36,8 @@ Add-Type -ErrorAction Stop -TypeDefinition @'
         WTSDown,
         WTSInit
     }
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
     public struct WTSINFOEX_LEVEL1_W {
         public Int32                  SessionId;
         public WTS_CONNECTSTATE_CLASS SessionState;
@@ -50,6 +60,66 @@ Add-Type -ErrorAction Stop -TypeDefinition @'
         public Int32            IncomingCompressedBytes;
         public Int32            OutgoingCompressedBytes;
     }
+    
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct WTSCONFIGINFOW {
+        public UInt32 version;
+        public UInt32 fConnectClientDrivesAtLogon;
+        public UInt32 fConnectPrinterAtLogon;
+        public UInt32 fDisablePrinterRedirection;
+        public UInt32 fDisableDefaultMainClientPrinter;
+        public UInt32 ShadowSettings;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 21)]   
+        public string  LogonUserName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 18)]   
+        public string  LogonDomain;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 261)]   
+        public string  WorkDirectory;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 261)]   
+        public string  InitialProgram;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 261)]   
+        public string  ApplicationName;  
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct WTSCLIENTW {
+      [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 21)]    
+      public string   ClientName;
+      [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 18)]
+      public string   Domain;
+      [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 21)]
+      public string   UserName;
+      [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 261)]
+      public string   WorkDirectory;
+      [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 261)]
+      public string   InitialProgram;
+      public byte   EncryptionLevel;
+      public UInt32  ClientAddressFamily;
+      [MarshalAs(UnmanagedType.ByValArray, SizeConst = 31)]
+      public UInt16[] ClientAddress;
+      public UInt16 HRes;
+      public UInt16 VRes;
+      public UInt16 ColorDepth;
+      [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 261)]
+      public string   ClientDirectory;
+      public UInt32  ClientBuildNumber;
+      public UInt32  ClientHardwareId;
+      public UInt16 ClientProductId;
+      public UInt16 OutBufCountHost;
+      public UInt16 OutBufCountClient;
+      public UInt16 OutBufLength;
+      [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 261)]
+      public string   DeviceId;
+    }
+        
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WTS_CLIENT_DISPLAY
+    {
+        public uint HorizontalResolution;
+        public uint VerticalResolution;
+        public uint ColorDepth;
+    }
+
     [StructLayout(LayoutKind.Sequential)]
     public struct WTS_SESSION_INFO
     {
@@ -149,10 +219,13 @@ Function Get-WTSSessionInformation
     [long]$ppBytesReturned = 0
     $wtsSessionInfo = New-Object -TypeName 'WTS_SESSION_INFO'
     $wtsInfoEx = New-Object -TypeName 'WTSINFOEX'
+    $wtsClientInfo = New-Object -TypeName 'WTSCLIENTW'
+    $wtsConfigInfo = New-Object -TypeName 'WTSCONFIGINFOW'
     [int]$datasize = [system.runtime.interopservices.marshal]::SizeOf( [Type]$wtsSessionInfo.GetType() )
 
     ForEach( $computer in $computers )
     {
+        $wtsinfo = $null
         [string]$machineName = $(if( $computer ) { $computer } else { $env:COMPUTERNAME })
         [IntPtr]$serverHandle = [wtsapi]::WTSOpenServer( $computer )
 
@@ -162,30 +235,31 @@ Function Get-WTSSessionInformation
 
         if ($retval -ne 0)
         {
+            Write-Verbose -Message "Got $count sessions for $computer"
              for ([int]$index = 0; $index -lt $count; $index++)
              {
-                 $element = [system.runtime.interopservices.marshal]::PtrToStructure( [long]$ppSessionInfo + ($datasize * $index), [type]$wtsSessionInfo.GetType())
-                 if( $element -and $element.SessionID -ne 0 ) ## session 0 is non-interactive (session zero isolation)
+                 ## session 0 is non-interactive (session zero isolation)
+                 if( ( $element = [system.runtime.interopservices.marshal]::PtrToStructure( [long]$ppSessionInfo + ($datasize * $index), [type]$wtsSessionInfo.GetType()) ) -and $element.SessionID -ne 0 )
                  {
-                     $retval = [wtsapi]::WTSQuerySessionInformationW( $serverHandle , $element.SessionID , [WTS_INFO_CLASS]::WTSSessionInfoEx , [ref]$ppQueryInfo , [ref]$ppBytesReturned );$LastError = [ComponentModel.Win32Exception][Runtime.InteropServices.Marshal]::GetLastWin32Error()
-                     if( $retval -and $ppQueryInfo )
+                    #$LastError = [ComponentModel.Win32Exception][Runtime.InteropServices.Marshal]::GetLastWin32Error()
+                     if( ( $retval = [wtsapi]::WTSQuerySessionInformationW( $serverHandle , $element.SessionID , [WTS_INFO_CLASS]::WTSSessionInfoEx , [ref]$ppQueryInfo , [ref]$ppBytesReturned ) -and $ppQueryInfo ) -and $ppQueryInfo )
                      {
-                        $value = [system.runtime.interopservices.marshal]::PtrToStructure( $ppQueryInfo , [Type]$wtsInfoEx.GetType())
-                        if( $value -and $value.Data -and $value.Data.WTSInfoExLevel1.SessionState -ne [WTS_CONNECTSTATE_CLASS]::WTSListen -and $value.Data.WTSInfoExLevel1.SessionState -ne [WTS_CONNECTSTATE_CLASS]::WTSConnected )
+                        if( ( $value = [system.runtime.interopservices.marshal]::PtrToStructure( $ppQueryInfo , [Type]$wtsInfoEx.GetType())) `
+                            -and $value.Data -and $value.Data.WTSInfoExLevel1.SessionState -ne [WTS_CONNECTSTATE_CLASS]::WTSListen -and $value.Data.WTSInfoExLevel1.SessionState -ne [WTS_CONNECTSTATE_CLASS]::WTSConnected )
                         {
                             $wtsinfo = $value.Data.WTSInfoExLevel1
                             $idleTime = New-TimeSpan -End ([datetime]::FromFileTimeUtc($wtsinfo.CurrentTime)) -Start ([datetime]::FromFileTimeUtc($wtsinfo.LastInputTime))
                             Add-Member -InputObject $wtsinfo -Force -NotePropertyMembers @{
-                                'IdleTimeInSeconds' =  $idleTime | Select -ExpandProperty TotalSeconds
-                                'IdleTimeInMinutes' =  $idleTime | Select -ExpandProperty TotalMinutes
+                                'IdleTimeInSeconds' =  [math]::Round( ( $idleTime | Select -ExpandProperty TotalSeconds ) , 1 )
+                                'IdleTimeInMinutes' =  [math]::Round( ( $idleTime | Select -ExpandProperty TotalMinutes ) , 2 )
                                 'Computer' = $machineName
                                 'LogonTime' = [datetime]::FromFileTime( $wtsinfo.LogonTime )
-                                'DisconnectTime' = [datetime]::FromFileTime( $wtsinfo.DisconnectTime )
+                                'DisconnectTime' = $( $time = [datetime]::FromFileTime( $wtsinfo.DisconnectTime ) ; if( $time.Year -lt 1900 ) { $null } else { $time })
                                 'LastInputTime' = [datetime]::FromFileTime( $wtsinfo.LastInputTime )
+                                'SessionState' = $wtsinfo.SessionState
                                 'ConnectTime' = [datetime]::FromFileTime( $wtsinfo.ConnectTime )
                                 'CurrentTime' = [datetime]::FromFileTime( $wtsinfo.CurrentTime )
                             }
-                            $wtsinfo
                         }
                         [wtsapi]::WTSFreeMemory( $ppQueryInfo )
                         $ppQueryInfo = [IntPtr]::Zero
@@ -193,6 +267,60 @@ Function Get-WTSSessionInformation
                      else
                      {
                         Write-Error "$($machineName): $LastError"
+                     }
+                     if( $wtsinfo )
+                     {
+                        ## WTSClientInfo
+                        $ppQueryInfo = [IntPtr]::Zero
+                        if( ( $retval = [wtsapi]::WTSQuerySessionInformationW( $serverHandle , $element.SessionID , [WTS_INFO_CLASS]::WTSClientInfo , [ref]$ppQueryInfo , [ref]$ppBytesReturned ) ) `
+                            -and $ppQueryInfo -and ( $wtsClientInfo = [system.runtime.interopservices.marshal]::PtrToStructure( $ppQueryInfo , [Type]$wtsClientInfo.GetType()) ) )
+                        {
+                            ForEach( $property in $wtsClientInfo.PSObject.Properties )
+                            {
+                                Add-Member -InputObject $wtsinfo -MemberType NoteProperty -Name $property.Name -Value $property.Value -Force
+                            }
+                           [wtsapi]::WTSFreeMemory( $ppQueryInfo )
+                           $ppQueryInfo = [IntPtr]::Zero
+                        }
+                        else
+                        {
+                            $LastError = [ComponentModel.Win32Exception][Runtime.InteropServices.Marshal]::GetLastWin32Error()
+                            Write-Warning -Message "Failed to get WTSClientInfo for session id $($element.SessionID)"
+                        }
+                        
+                        ## WTSConfigInfo
+                        $ppQueryInfo = [IntPtr]::Zero
+                        if( ( $retval = [wtsapi]::WTSQuerySessionInformationW( $serverHandle , $element.SessionID , [WTS_INFO_CLASS]::WTSConfigInfo , [ref]$ppQueryInfo , [ref]$ppBytesReturned ) ) `
+                            -and $ppQueryInfo -and ( $wtsConfigInfo = [system.runtime.interopservices.marshal]::PtrToStructure( $ppQueryInfo , [Type]$wtsConfigInfo.GetType()) ) )
+                        {
+                            ForEach( $property in $wtsConfigInfo.PSObject.Properties )
+                            {
+                                ## WorkDirectory and InitialProgram don't seem to work and we have no new strings here so don't add string type properties
+                                if( $property.TypeNameOfValue -ne 'System.String' )
+                                {
+                                    Add-Member -InputObject $wtsinfo -MemberType NoteProperty -Name $property.Name -Value $property.Value -Force
+                                }
+                            }
+                           [wtsapi]::WTSFreeMemory( $ppQueryInfo )
+                           $ppQueryInfo = [IntPtr]::Zero
+                        }
+                        else
+                        {
+                            $LastError = [ComponentModel.Win32Exception][Runtime.InteropServices.Marshal]::GetLastWin32Error()
+                            Write-Warning -Message "Failed to get WTSConfigInfo for session id $($element.SessionID)"
+                        }
+
+                        [UInt16]$clientProtocolType = ([UInt16]::MaxValue)
+                        
+                        if( ( $retval = [wtsapi]::WTSQuerySessionInformationW( $serverHandle , $element.SessionID , [WTS_INFO_CLASS]::WTSClientProtocolType , [ref]$ppQueryInfo , [ref]$ppBytesReturned ) ) -and $ppQueryInfo )
+                        {
+                            $clientProtocolType = [system.runtime.interopservices.marshal]::PtrToStructure( $ppQueryInfo , [Type]$clientProtocolType.GetType())
+                            Add-Member -InputObject $wtsinfo -MemberType NoteProperty -Name ClientProtocolType -Value $clientProtocolType
+                            [wtsapi]::WTSFreeMemory( $ppQueryInfo )
+                            $ppQueryInfo = [IntPtr]::Zero
+                        }
+                        $wtsinfo
+                        $wtsinfo = $null
                      }
                  }
              }
