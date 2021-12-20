7386d59f49969a0c07c997d3fa695df4bd051e38
Guy Leech <guyrleech@gmail.com>
2021-12-20 23:45:52 +0000

    Set registry key ownership & full control

---
+++ Set Key Owner.ps1
@@ -0,0 +1,353 @@
+﻿<#
+.SYNOPSIS
+    Set the owner of a specified registry key and give that account full control of it
+
+.PARAMETER path
+    The registry key to operate on
+
+.PARAMETER owner
+    The account to set ownership to and give full control
+
+.EXAMPLE
+    & '.\Set Key Owner.ps1' -path 'HKEY_LOCAL_MACHINE\Software\Guy Leech' -owner guyrleech\beeblebrox
+
+    Set the owner of the specified key to the specified account
+
+.NOTES
+    PowerShell process needs to have the take ownership privilege which it will try and get
+
+    Modification History:
+
+    2021/12/20  @guyrleech  Initial release
+#>
+
+[CmdletBinding()]
+
+Param
+(
+    [Parameter(Mandatory=$true,ValueFromPipeline=$true,HelpMessage='Registry key to operate on')]
+    [string]$path ,
+    [Parameter(Mandatory=$false,HelpMessage='Account to set ownership and full control to')]
+    [string]$owner
+)
+
+Begin
+{
+    if( [string]::IsNullOrEmpty( $owner ) )
+    {
+        $owner = "$env:USERDOMAIN\$env:USERNAME"
+    }
+
+    if( -Not ( $user = New-Object -Typename System.Security.Principal.NTAccount( $owner ) ))
+    {
+        Throw "Failed to get account $owner"
+    }
+
+    ## from https://www.leeholmes.com/blog/2010/09/24/adjusting-token-privileges-in-powershell/
+    Function Set-TokenPrivileges
+    {
+        [CmdletBinding()]
+
+        param(    ## The privilege to adjust. This set is taken from
+            ## http://msdn.microsoft.com/en-us/library/bb530716(VS.85).aspx
+
+            [ValidateSet(
+                "SeAssignPrimaryTokenPrivilege", "SeAuditPrivilege", "SeBackupPrivilege",
+                "SeChangeNotifyPrivilege", "SeCreateGlobalPrivilege", "SeCreatePagefilePrivilege",
+                "SeCreatePermanentPrivilege", "SeCreateSymbolicLinkPrivilege", "SeCreateTokenPrivilege",
+                "SeDebugPrivilege", "SeEnableDelegationPrivilege", "SeImpersonatePrivilege", "SeIncreaseBasePriorityPrivilege",
+                "SeIncreaseQuotaPrivilege", "SeIncreaseWorkingSetPrivilege", "SeLoadDriverPrivilege",
+                "SeLockMemoryPrivilege", "SeMachineAccountPrivilege", "SeManageVolumePrivilege",
+                "SeProfileSingleProcessPrivilege", "SeRelabelPrivilege", "SeRemoteShutdownPrivilege",
+                "SeRestorePrivilege", "SeSecurityPrivilege", "SeShutdownPrivilege", "SeSyncAgentPrivilege",
+                "SeSystemEnvironmentPrivilege", "SeSystemProfilePrivilege", "SeSystemtimePrivilege",
+                "SeTakeOwnershipPrivilege", "SeTcbPrivilege", "SeTimeZonePrivilege", "SeTrustedCredManAccessPrivilege",
+                "SeUndockPrivilege", "SeUnsolicitedInputPrivilege" , "SeServiceLogonRight" )]
+            [Parameter(Mandatory=$true)]
+            $Privilege,
+
+            ## The process on which to adjust the privilege. Defaults to the current process.
+            $ProcessId = $pid,
+
+            ## Switch to disable the privilege, rather than enable it.
+            [Switch] $Disable
+        )
+
+        ## Taken from P/Invoke.NET with minor adjustments. 
+
+        $definition = @'
+        using System;
+        using System.Runtime.InteropServices;
+
+        public class AdjPriv
+        {
+            [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+
+            internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall,
+
+            ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
+            [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+
+            internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr phtok);
+            [DllImport("advapi32.dll", SetLastError = true)]
+
+            internal static extern bool LookupPrivilegeValue(string host, string name, ref long pluid);
+            [StructLayout(LayoutKind.Sequential, Pack = 1)]
+
+            internal struct TokPriv1Luid
+            {
+                public int Count;
+                public long Luid;
+                public int Attr;
+            }
+            internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
+
+            internal const int SE_PRIVILEGE_DISABLED = 0x00000000;
+
+            internal const int TOKEN_QUERY = 0x00000008;
+
+            internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+            public static bool EnablePrivilege(long processHandle, string privilege, bool disable)
+            {
+                bool retVal = false;
+                TokPriv1Luid tp;
+                IntPtr hproc = new IntPtr(processHandle);
+                IntPtr htok = IntPtr.Zero;
+
+                retVal = OpenProcessToken(hproc, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok);
+
+                tp.Count = 1;
+
+                tp.Luid = 0;
+                if(disable)
+                {
+                    tp.Attr = SE_PRIVILEGE_DISABLED;
+                }
+                else
+                {
+                    tp.Attr = SE_PRIVILEGE_ENABLED;
+                }
+                retVal = LookupPrivilegeValue(null, privilege, ref tp.Luid);
+
+                retVal = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+
+                return retVal;
+            }
+        }
+'@
+
+        if( ( $processHandle = (Get-Process -id $ProcessId -ErrorAction Stop).Handle ) -and ($type = Add-Type $definition -PassThru) )
+        {
+            $type[0]::EnablePrivilege($processHandle, $Privilege, $Disable)
+        }
+        else
+        {
+            Throw "Failed to get process handle"
+        }
+    }
+}
+
+Process
+{
+    [hashtable]$keyBases = @{
+        'HKLM' = [Microsoft.Win32.Registry]::LocalMachine
+        'HKEY_LOCAL_MACHINE' = [Microsoft.Win32.Registry]::LocalMachine
+        'HKCR' = [Microsoft.Win32.Registry]::ClassesRoot
+        'HKEY_CLASSES_ROOT' = [Microsoft.Win32.Registry]::ClassesRoot
+        'HKCU' = [Microsoft.Win32.Registry]::CurrentUser
+        'HKEY_CURRENT_USER' = [Microsoft.Win32.Registry]::CurrentUser
+        'HKCC' = [Microsoft.Win32.Registry]::CurrentConfig
+        'HKEY_CURRENT_CONFIG' = [Microsoft.Win32.Registry]::CurrentConfig
+        'HKU' = [Microsoft.Win32.Registry]::Users
+        'HKEY_USERS' = [Microsoft.Win32.Registry]::Users
+    }
+
+    [string]$subpath = $null
+    $baseKey = $null
+
+    ## need to parse key so that we get the base/hive and subkey separately
+    if( $path -match '^(HK[^:]+):\\(.*)$' )
+    {
+        if( -Not ( $baseKey = $keyBases[ $Matches[ 1 ] ] ) )
+        {
+            Throw "Unrecognised base key `"$($Matches[1])`" in $path"
+        }
+        $subpath = $Matches[ 2 ]
+    }
+    else
+    {
+        Throw "Unexpected base key in $path"
+    }
+
+    if( -Not ( Set-TokenPrivileges -Privilege SeTakeOwnershipPrivilege -ProcessId $pid ) )
+    {
+        Write-Warning -Message "Failed to get take ownership privilege"
+    }
+
+    if( $key = $baseKey.OpenSubKey( $subpath , [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree , [System.Security.AccessControl.RegistryRights]::TakeOwnership ) )
+    {
+        $acl = $key.GetAccessControl()
+        $acl.SetOwner( $user )
+        $key.SetAccessControl( $acl )
+
+        if( $rule = New-Object -TypeName System.Security.AccessControl.RegistryAccessRule(
+            $user, 
+            [System.Security.AccessControl.RegistryRights]::FullControl ,
+            [System.Security.AccessControl.InheritanceFlags]'ObjectInherit,ContainerInherit',
+            [System.Security.AccessControl.PropagationFlags]::None ,
+            [System.Security.AccessControl.AccessControlType]::Allow ) )
+        {
+            $acl.AddAccessRule( $rule )
+            $key.SetAccessControl( $acl )
+        }
+        else
+        {
+            Write-Warning -Message "Failed to create ACE for $user"
+        }
+    }
+    else
+    {
+        Write-Warning -Message "Failed to open key $path"
+    }
+}
+
+# SIG # Begin signature block
+# MIIZsAYJKoZIhvcNAQcCoIIZoTCCGZ0CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUswByuNq/dtT/eQiUPu3nVoLP
+# w5WgghS+MIIE/jCCA+agAwIBAgIQDUJK4L46iP9gQCHOFADw3TANBgkqhkiG9w0B
+# AQsFADByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
+# VQQLExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFz
+# c3VyZWQgSUQgVGltZXN0YW1waW5nIENBMB4XDTIxMDEwMTAwMDAwMFoXDTMxMDEw
+# NjAwMDAwMFowSDELMAkGA1UEBhMCVVMxFzAVBgNVBAoTDkRpZ2lDZXJ0LCBJbmMu
+# MSAwHgYDVQQDExdEaWdpQ2VydCBUaW1lc3RhbXAgMjAyMTCCASIwDQYJKoZIhvcN
+# AQEBBQADggEPADCCAQoCggEBAMLmYYRnxYr1DQikRcpja1HXOhFCvQp1dU2UtAxQ
+# tSYQ/h3Ib5FrDJbnGlxI70Tlv5thzRWRYlq4/2cLnGP9NmqB+in43Stwhd4CGPN4
+# bbx9+cdtCT2+anaH6Yq9+IRdHnbJ5MZ2djpT0dHTWjaPxqPhLxs6t2HWc+xObTOK
+# fF1FLUuxUOZBOjdWhtyTI433UCXoZObd048vV7WHIOsOjizVI9r0TXhG4wODMSlK
+# XAwxikqMiMX3MFr5FK8VX2xDSQn9JiNT9o1j6BqrW7EdMMKbaYK02/xWVLwfoYer
+# vnpbCiAvSwnJlaeNsvrWY4tOpXIc7p96AXP4Gdb+DUmEvQECAwEAAaOCAbgwggG0
+# MA4GA1UdDwEB/wQEAwIHgDAMBgNVHRMBAf8EAjAAMBYGA1UdJQEB/wQMMAoGCCsG
+# AQUFBwMIMEEGA1UdIAQ6MDgwNgYJYIZIAYb9bAcBMCkwJwYIKwYBBQUHAgEWG2h0
+# dHA6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzAfBgNVHSMEGDAWgBT0tuEgHf4prtLk
+# YaWyoiWyyBc1bjAdBgNVHQ4EFgQUNkSGjqS6sGa+vCgtHUQ23eNqerwwcQYDVR0f
+# BGowaDAyoDCgLoYsaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL3NoYTItYXNzdXJl
+# ZC10cy5jcmwwMqAwoC6GLGh0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9zaGEyLWFz
+# c3VyZWQtdHMuY3JsMIGFBggrBgEFBQcBAQR5MHcwJAYIKwYBBQUHMAGGGGh0dHA6
+# Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBPBggrBgEFBQcwAoZDaHR0cDovL2NhY2VydHMu
+# ZGlnaWNlcnQuY29tL0RpZ2lDZXJ0U0hBMkFzc3VyZWRJRFRpbWVzdGFtcGluZ0NB
+# LmNydDANBgkqhkiG9w0BAQsFAAOCAQEASBzctemaI7znGucgDo5nRv1CclF0CiNH
+# o6uS0iXEcFm+FKDlJ4GlTRQVGQd58NEEw4bZO73+RAJmTe1ppA/2uHDPYuj1UUp4
+# eTZ6J7fz51Kfk6ftQ55757TdQSKJ+4eiRgNO/PT+t2R3Y18jUmmDgvoaU+2QzI2h
+# F3MN9PNlOXBL85zWenvaDLw9MtAby/Vh/HUIAHa8gQ74wOFcz8QRcucbZEnYIpp1
+# FUL1LTI4gdr0YKK6tFL7XOBhJCVPst/JKahzQ1HavWPWH1ub9y4bTxMd90oNcX6X
+# t/Q/hOvB46NJofrOp79Wz7pZdmGJX36ntI5nePk2mOHLKNpbh6aKLzCCBTAwggQY
+# oAMCAQICEAQJGBtf1btmdVNDtW+VUAgwDQYJKoZIhvcNAQELBQAwZTELMAkGA1UE
+# BhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2lj
+# ZXJ0LmNvbTEkMCIGA1UEAxMbRGlnaUNlcnQgQXNzdXJlZCBJRCBSb290IENBMB4X
+# DTEzMTAyMjEyMDAwMFoXDTI4MTAyMjEyMDAwMFowcjELMAkGA1UEBhMCVVMxFTAT
+# BgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEx
+# MC8GA1UEAxMoRGlnaUNlcnQgU0hBMiBBc3N1cmVkIElEIENvZGUgU2lnbmluZyBD
+# QTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAPjTsxx/DhGvZ3cH0wsx
+# SRnP0PtFmbE620T1f+Wondsy13Hqdp0FLreP+pJDwKX5idQ3Gde2qvCchqXYJawO
+# eSg6funRZ9PG+yknx9N7I5TkkSOWkHeC+aGEI2YSVDNQdLEoJrskacLCUvIUZ4qJ
+# RdQtoaPpiCwgla4cSocI3wz14k1gGL6qxLKucDFmM3E+rHCiq85/6XzLkqHlOzEc
+# z+ryCuRXu0q16XTmK/5sy350OTYNkO/ktU6kqepqCquE86xnTrXE94zRICUj6whk
+# PlKWwfIPEvTFjg/BougsUfdzvL2FsWKDc0GCB+Q4i2pzINAPZHM8np+mM6n9Gd8l
+# k9ECAwEAAaOCAc0wggHJMBIGA1UdEwEB/wQIMAYBAf8CAQAwDgYDVR0PAQH/BAQD
+# AgGGMBMGA1UdJQQMMAoGCCsGAQUFBwMDMHkGCCsGAQUFBwEBBG0wazAkBggrBgEF
+# BQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tMEMGCCsGAQUFBzAChjdodHRw
+# Oi8vY2FjZXJ0cy5kaWdpY2VydC5jb20vRGlnaUNlcnRBc3N1cmVkSURSb290Q0Eu
+# Y3J0MIGBBgNVHR8EejB4MDqgOKA2hjRodHRwOi8vY3JsNC5kaWdpY2VydC5jb20v
+# RGlnaUNlcnRBc3N1cmVkSURSb290Q0EuY3JsMDqgOKA2hjRodHRwOi8vY3JsMy5k
+# aWdpY2VydC5jb20vRGlnaUNlcnRBc3N1cmVkSURSb290Q0EuY3JsME8GA1UdIARI
+# MEYwOAYKYIZIAYb9bAACBDAqMCgGCCsGAQUFBwIBFhxodHRwczovL3d3dy5kaWdp
+# Y2VydC5jb20vQ1BTMAoGCGCGSAGG/WwDMB0GA1UdDgQWBBRaxLl7KgqjpepxA8Bg
+# +S32ZXUOWDAfBgNVHSMEGDAWgBRF66Kv9JLLgjEtUYunpyGd823IDzANBgkqhkiG
+# 9w0BAQsFAAOCAQEAPuwNWiSz8yLRFcgsfCUpdqgdXRwtOhrE7zBh134LYP3DPQ/E
+# r4v97yrfIFU3sOH20ZJ1D1G0bqWOWuJeJIFOEKTuP3GOYw4TS63XX0R58zYUBor3
+# nEZOXP+QsRsHDpEV+7qvtVHCjSSuJMbHJyqhKSgaOnEoAjwukaPAJRHinBRHoXpo
+# aK+bp1wgXNlxsQyPu6j4xRJon89Ay0BEpRPw5mQMJQhCMrI2iiQC/i9yfhzXSUWW
+# 6Fkd6fp0ZGuy62ZD2rOwjNXpDd32ASDOmTFjPQgaGLOBm0/GkxAG/AeB+ova+YJJ
+# 92JuoVP6EpQYhS6SkepobEQysmah5xikmmRR7zCCBTEwggQZoAMCAQICEAqhJdbW
+# Mht+QeQF2jaXwhUwDQYJKoZIhvcNAQELBQAwZTELMAkGA1UEBhMCVVMxFTATBgNV
+# BAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEkMCIG
+# A1UEAxMbRGlnaUNlcnQgQXNzdXJlZCBJRCBSb290IENBMB4XDTE2MDEwNzEyMDAw
+# MFoXDTMxMDEwNzEyMDAwMFowcjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lD
+# ZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTExMC8GA1UEAxMoRGln
+# aUNlcnQgU0hBMiBBc3N1cmVkIElEIFRpbWVzdGFtcGluZyBDQTCCASIwDQYJKoZI
+# hvcNAQEBBQADggEPADCCAQoCggEBAL3QMu5LzY9/3am6gpnFOVQoV7YjSsQOB0Uz
+# URB90Pl9TWh+57ag9I2ziOSXv2MhkJi/E7xX08PhfgjWahQAOPcuHjvuzKb2Mln+
+# X2U/4Jvr40ZHBhpVfgsnfsCi9aDg3iI/Dv9+lfvzo7oiPhisEeTwmQNtO4V8CdPu
+# XciaC1TjqAlxa+DPIhAPdc9xck4Krd9AOly3UeGheRTGTSQjMF287DxgaqwvB8z9
+# 8OpH2YhQXv1mblZhJymJhFHmgudGUP2UKiyn5HU+upgPhH+fMRTWrdXyZMt7HgXQ
+# hBlyF/EXBu89zdZN7wZC/aJTKk+FHcQdPK/P2qwQ9d2srOlW/5MCAwEAAaOCAc4w
+# ggHKMB0GA1UdDgQWBBT0tuEgHf4prtLkYaWyoiWyyBc1bjAfBgNVHSMEGDAWgBRF
+# 66Kv9JLLgjEtUYunpyGd823IDzASBgNVHRMBAf8ECDAGAQH/AgEAMA4GA1UdDwEB
+# /wQEAwIBhjATBgNVHSUEDDAKBggrBgEFBQcDCDB5BggrBgEFBQcBAQRtMGswJAYI
+# KwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBDBggrBgEFBQcwAoY3
+# aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9v
+# dENBLmNydDCBgQYDVR0fBHoweDA6oDigNoY0aHR0cDovL2NybDQuZGlnaWNlcnQu
+# Y29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNybDA6oDigNoY0aHR0cDovL2Ny
+# bDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNybDBQBgNV
+# HSAESTBHMDgGCmCGSAGG/WwAAgQwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cu
+# ZGlnaWNlcnQuY29tL0NQUzALBglghkgBhv1sBwEwDQYJKoZIhvcNAQELBQADggEB
+# AHGVEulRh1Zpze/d2nyqY3qzeM8GN0CE70uEv8rPAwL9xafDDiBCLK938ysfDCFa
+# KrcFNB1qrpn4J6JmvwmqYN92pDqTD/iy0dh8GWLoXoIlHsS6HHssIeLWWywUNUME
+# aLLbdQLgcseY1jxk5R9IEBhfiThhTWJGJIdjjJFSLK8pieV4H9YLFKWA1xJHcLN1
+# 1ZOFk362kmf7U2GJqPVrlsD0WGkNfMgBsbkodbeZY4UijGHKeZR+WfyMD+NvtQEm
+# tmyl7odRIeRYYJu6DC0rbaLEfrvEJStHAgh8Sa4TtuF8QkIoxhhWz0E0tmZdtnR7
+# 9VYzIi8iNrJLokqV2PWmjlIwggVPMIIEN6ADAgECAhAE/eOq2921q55B9NnVIXVO
+# MA0GCSqGSIb3DQEBCwUAMHIxCzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2Vy
+# dCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20xMTAvBgNVBAMTKERpZ2lD
+# ZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25pbmcgQ0EwHhcNMjAwNzIwMDAw
+# MDAwWhcNMjMwNzI1MTIwMDAwWjCBizELMAkGA1UEBhMCR0IxEjAQBgNVBAcTCVdh
+# a2VmaWVsZDEmMCQGA1UEChMdU2VjdXJlIFBsYXRmb3JtIFNvbHV0aW9ucyBMdGQx
+# GDAWBgNVBAsTD1NjcmlwdGluZ0hlYXZlbjEmMCQGA1UEAxMdU2VjdXJlIFBsYXRm
+# b3JtIFNvbHV0aW9ucyBMdGQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
+# AQCvbSdd1oAAu9rTtdnKSlGWKPF8g+RNRAUDFCBdNbYbklzVhB8hiMh48LqhoP7d
+# lzZY3YmuxztuPlB7k2PhAccd/eOikvKDyNeXsSa3WaXLNSu3KChDVekEFee/vR29
+# mJuujp1eYrz8zfvDmkQCP/r34Bgzsg4XPYKtMitCO/CMQtI6Rnaj7P6Kp9rH1nVO
+# /zb7KD2IMedTFlaFqIReT0EVG/1ZizOpNdBMSG/x+ZQjZplfjyyjiYmE0a7tWnVM
+# Z4KKTUb3n1CTuwWHfK9G6CNjQghcFe4D4tFPTTKOSAx7xegN1oGgifnLdmtDtsJU
+# OOhOtyf9Kp8e+EQQyPVrV/TNAgMBAAGjggHFMIIBwTAfBgNVHSMEGDAWgBRaxLl7
+# KgqjpepxA8Bg+S32ZXUOWDAdBgNVHQ4EFgQUTXqi+WoiTm5fYlDLqiDQ4I+uyckw
+# DgYDVR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMHcGA1UdHwRwMG4w
+# NaAzoDGGL2h0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9zaGEyLWFzc3VyZWQtY3Mt
+# ZzEuY3JsMDWgM6Axhi9odHRwOi8vY3JsNC5kaWdpY2VydC5jb20vc2hhMi1hc3N1
+# cmVkLWNzLWcxLmNybDBMBgNVHSAERTBDMDcGCWCGSAGG/WwDATAqMCgGCCsGAQUF
+# BwIBFhxodHRwczovL3d3dy5kaWdpY2VydC5jb20vQ1BTMAgGBmeBDAEEATCBhAYI
+# KwYBBQUHAQEEeDB2MCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5j
+# b20wTgYIKwYBBQUHMAKGQmh0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdp
+# Q2VydFNIQTJBc3N1cmVkSURDb2RlU2lnbmluZ0NBLmNydDAMBgNVHRMBAf8EAjAA
+# MA0GCSqGSIb3DQEBCwUAA4IBAQBT3M71SlOQ8vwM2txshp/XDvfoKBYHkpFCyanW
+# aFdsYQJQIKk4LOVgUJJ6LAf0xPSN7dZpjFaoilQy8Ajyd0U9UOnlEX4gk2J+z5i4
+# sFxK/W2KU1j6R9rY5LbScWtsV+X1BtHihpzPywGGE5eth5Q5TixMdI9CN3eWnKGF
+# kY13cI69zZyyTnkkb+HaFHZ8r6binvOyzMr69+oRf0Bv/uBgyBKjrmGEUxJZy+00
+# 7fbmYDEclgnWT1cRROarzbxmZ8R7Iyor0WU3nKRgkxan+8rzDhzpZdtgIFdYvjeO
+# c/IpPi2mI6NY4jqDXwkx1TEIbjUdrCmEfjhAfMTU094L7VSNMYIEXDCCBFgCAQEw
+# gYYwcjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UE
+# CxMQd3d3LmRpZ2ljZXJ0LmNvbTExMC8GA1UEAxMoRGlnaUNlcnQgU0hBMiBBc3N1
+# cmVkIElEIENvZGUgU2lnbmluZyBDQQIQBP3jqtvdtaueQfTZ1SF1TjAJBgUrDgMC
+# GgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYK
+# KwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG
+# 9w0BCQQxFgQUPmdNds+wuJ4R/VTGZe2uRvJVfqkwDQYJKoZIhvcNAQEBBQAEggEA
+# h/hACF8F/XueZUilhMWPOnCuN/4dkGOPY+7uFE8QjNiHrGVXH05+wR8XVIuG6w6H
+# YWIbNn4MCmTJxjcJ1oMvFKR8g7Tpn2LLNPCQhwaIpVzUXJdcaK228OVyCp2FcA1d
+# uzDcUt6damwV9IXQ1CCU+BtjHHjjPKMl6BIEwfHLQXVX3khTQvUJcQDrZRDYa2ij
+# 31Z6h+aul3wigfHno2VN32/L6/04llbqyzwyozCEfWNM7uUZ+DIfkSrnFza/P1Ks
+# 8y0F0B0qPzXUz8FtNVoFeJp4ESuGPO4c3mJx11zptb3DlyD0yCOpo8EdZQoMYvtN
+# 5pGlZDCbJk3lrEhRpgYlRaGCAjAwggIsBgkqhkiG9w0BCQYxggIdMIICGQIBATCB
+# hjByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQL
+# ExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3Vy
+# ZWQgSUQgVGltZXN0YW1waW5nIENBAhANQkrgvjqI/2BAIc4UAPDdMA0GCWCGSAFl
+# AwQCAQUAoGkwGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUx
+# DxcNMjExMjIwMjM0NDU0WjAvBgkqhkiG9w0BCQQxIgQgvJXUrK55kxC4NNpdLMRo
+# 9Fr/wEsK9eS7KLkxScISDoEwDQYJKoZIhvcNAQEBBQAEggEAeQ+1m02COIjwqwPn
+# mC8KOrgQt3Wxwd2C2SiTxoDTPpvckMcADtFI+tRItu++wYojbH0rND3vImE71675
+# 2mDrotvwlTgZ1UOoyyOl3Kiccmjcd1OxL+aql4Xxkpnb/f4NKTeNFKdwIIHZYBRl
+# T2k2RLUaMgtvYciV1bjl8cfi+KBvHMeL299eN4Zjzt12taxinDWp+BKeRGdb63Oz
+# X/3yWZbmMj/yqToRUew+LnepetQbC19309jJu/EOYtVDEgH+LhdxVKQbve9jnshM
+# f0t9ViRFvsaunTzUK0ZOt1djlkt6MAVd+SsaBjWDSVS4wusU3lTRis8SLqTza1J2
+# QImoWQ==
+# SIG # End signature block
