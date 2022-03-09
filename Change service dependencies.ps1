e6177b7e605fccfc9ad6be56d03cd2d69235458f
Guy Leech <guyrleech@gmail.com>
2022-03-09 09:04:08 +0000

    Show, add or remove Windows services dependencies

---
+++ Change service dependencies.ps1
@@ -0,0 +1,341 @@
+﻿#requires -version 3
+
+<#
+.SYNOPSIS
+    Add, remove or show service dependencies for one or more services
+
+.PARAMETER services
+    Services to view/modify. Can be specified by name or display name, the default being name unless -property DisplayName used
+
+.PARAMETER add
+    List of service depedencies to add to what is already present. Can be specified by name or display name, the default being name unless -property DisplayName used
+
+.PARAMETER remove
+    List of service dependencies to remove. Can be specified by name or display name, the default being name unless -property DisplayName used
+    Use * to remove all existing dependecies
+
+.PARAMETER property
+    The property to use to identify services specified by the other parameters
+
+.EXAMPLE
+    & '.\Change service dependencies.ps1' -services brokeragent,stackcontrolservice
+
+    Show existing service dependencies for the Citrix Desktop Service and the Citrix Stack Control Service
+
+.EXAMPLE
+    & '.\Change service dependencies.ps1' -services brokeragent,stackcontrolservice  -whatif -remove *
+
+    See what services would be removed if all were removed via the * wildcard
+
+.EXAMPLE
+    & '.\Change service dependencies.ps1' -services brokeragent,stackcontrolservice -remove termservices
+
+.EXAMPLE
+    & '.\Change service dependencies.ps1' -property displayname -services "Citrix Desktop Service","Citrix Stack Control Service" -add "Remote Desktop Services"
+
+.EXAMPLE
+    & '.\Change service dependencies.ps1' -services brokeragent,stackcontrolservice -remove termservices -add workstation -confirm:$false
+
+.NOTES
+    No other wildcards other than the use of * on its own have been impemented meaning that service names must be specified exactly
+
+    Modification History:
+
+    @guyrleech 2022/03/08 Created
+#>
+
+<#
+Copyright © 2022 Guy Leech
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#>
+
+[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='High')]
+
+Param
+(
+    [string[]]$services ,
+    [string[]]$add ,
+    [string[]]$remove ,
+    [ValidateSet('Name','DisplayName')]
+    [string]$property = 'Name'
+)
+
+if( $null -ne $add -and  ( $add | Sort-Object -Unique | Measure-Object).Count -ne $add.Count )
+{
+    Throw "Duplication in add list"
+}
+
+if( $null -ne $remove -and ( $remove | Sort-Object -Unique | Measure-Object).Count -ne $remove.Count )
+{
+    Throw "Duplication in remove list"
+}
+
+$additions = New-Object -TypeName System.Collections.Generic.List[string]
+
+ForEach( $addition in $add )
+{
+    if( -Not ( $service = Get-CimInstance -ClassName win32_service -Filter "$property = '$addition'" -ErrorAction SilentlyContinue ) )
+    {
+        Throw "Unable to find dependent service $addition"
+    }
+    else
+    {
+        $additions.Add( $service.Name )
+    }
+}
+
+$removals = New-Object -TypeName System.Collections.Generic.List[string]
+
+ForEach( $removal in $remove )
+{
+    if( $remove -ne '*' )
+    {
+        if( -Not ( $service = Get-CimInstance -ClassName win32_service -Filter "$property = '$removal'" -ErrorAction SilentlyContinue ) )
+        {
+            Throw "Unable to find dependent service $removal"
+        }
+        else
+        {
+            $removals.Add( $service.Name )
+        }
+    }
+    else
+    {
+        $removals.Add( '*' )
+    }
+}
+
+ForEach( $service in $services )
+{
+    if( $thisService = Get-CimInstance -ClassName win32_service -Filter "$property = '$service'" )
+    {
+        [string[]]$currentDependencies = @( Get-CimAssociatedInstance -InputObject $thisService -ResultClassName win32_service | Select-Object -ExpandProperty Name )
+
+        if( ( $null -eq $add -and $null -eq $remove ) -or ( $add -and $add.Count -eq 0 -and $remove -and $remove.Count -eq 0 ))
+        {
+            [pscustomobject]@{
+                Service = $thisService.Name
+                DisplayName = $thisService.DisplayName
+                Dependencies = $currentDependencies
+            }
+        }
+        else
+        {
+            Write-Verbose -Message "$($thisService.Name) has $($currentDependencies.Count) dependencies - $($currentDependencies -join ',')"
+
+            [System.Collections.Generic.List[string]]$newDependencies = $currentDependencies
+            ForEach( $addition in $additions )
+            {
+                if( $newDependencies | Where-Object { $_ -ieq $addition } )
+                {
+                    Write-Warning -Message "$service already has a dependency on $addition"
+                }
+                else
+                {
+                    $newDependencies.Add( $addition )
+                }
+            }
+            ForEach( $removal in $removals )
+            {
+                if( $removal -eq '*' )
+                {
+                    [int]$countBefore  = $newDependencies.Count
+                    [int]$countRemoved = $newDependencies.RemoveAll( { Param( $item ) $item -match '.?' } )
+                    if( $countRemoved -ne $countBefore )
+                    {
+                        Throw "Removal of * removed $countRemoved items not all $countBefore"
+                    }
+                }
+                else
+                {
+                    ## can't use Remove() method as it is case sensitive
+                    [bool]$found = $false
+                    For( [int]$index = $newDependencies.Count - 1 ; $index -ge 0 ; $index-- ) ## work backwards since we could be removing items from the array whilst iterating over it
+                    {
+                        if( $newDependencies[ $index ] -ieq $removal )
+                        {
+                            try
+                            {
+                                $newDependencies.RemoveAt( $index )
+                                $found = $true
+                            }
+                            catch
+                            {
+                                Throw "Error removing $removal at index $index ($($newDependencies.Count) items) for $service"
+                            }
+                        }
+                    }
+                    if( -Not $found )
+                    {
+                        Write-Warning -Message "$service does not have a dependency on $remove"
+                    }
+                }
+            }
+            if( $PSCmdlet.ShouldProcess( "Service $service" , "Set dependencies to $($newDependencies -join ' , ')" ) )
+            {
+                $result = $null
+                if( $newDependencies.Count -eq 0 )
+                {
+                    $newDependencies = @( '' ) ## passing an empty array doesn't change the dependencies
+                }
+                $result = Invoke-CimMethod -InputObject $thisService -MethodName Change -Arguments @{ 'ServiceDependencies' = $newDependencies }
+                if( -Not $result -or $result.ReturnValue -ne 0 )
+                {
+                    Write-Error -Message "Error setting dependencies to $($newDependencies -join ',') for $service - $($result | Select-Object -ExpandProperty ReturnValue -ErrorAction SilentlyContinue)"
+                }
+            }
+        }
+    }
+    else
+    {
+        Write-Warning -Message "Unable to get service $service"
+    }
+}
+
+# SIG # Begin signature block
+# MIIZsAYJKoZIhvcNAQcCoIIZoTCCGZ0CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUgFQBivnCAGOeiMyPnrR/jaXW
+# 4sOgghS+MIIE/jCCA+agAwIBAgIQDUJK4L46iP9gQCHOFADw3TANBgkqhkiG9w0B
+# AQsFADByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
+# VQQLExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFz
+# c3VyZWQgSUQgVGltZXN0YW1waW5nIENBMB4XDTIxMDEwMTAwMDAwMFoXDTMxMDEw
+# NjAwMDAwMFowSDELMAkGA1UEBhMCVVMxFzAVBgNVBAoTDkRpZ2lDZXJ0LCBJbmMu
+# MSAwHgYDVQQDExdEaWdpQ2VydCBUaW1lc3RhbXAgMjAyMTCCASIwDQYJKoZIhvcN
+# AQEBBQADggEPADCCAQoCggEBAMLmYYRnxYr1DQikRcpja1HXOhFCvQp1dU2UtAxQ
+# tSYQ/h3Ib5FrDJbnGlxI70Tlv5thzRWRYlq4/2cLnGP9NmqB+in43Stwhd4CGPN4
+# bbx9+cdtCT2+anaH6Yq9+IRdHnbJ5MZ2djpT0dHTWjaPxqPhLxs6t2HWc+xObTOK
+# fF1FLUuxUOZBOjdWhtyTI433UCXoZObd048vV7WHIOsOjizVI9r0TXhG4wODMSlK
+# XAwxikqMiMX3MFr5FK8VX2xDSQn9JiNT9o1j6BqrW7EdMMKbaYK02/xWVLwfoYer
+# vnpbCiAvSwnJlaeNsvrWY4tOpXIc7p96AXP4Gdb+DUmEvQECAwEAAaOCAbgwggG0
+# MA4GA1UdDwEB/wQEAwIHgDAMBgNVHRMBAf8EAjAAMBYGA1UdJQEB/wQMMAoGCCsG
+# AQUFBwMIMEEGA1UdIAQ6MDgwNgYJYIZIAYb9bAcBMCkwJwYIKwYBBQUHAgEWG2h0
+# dHA6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzAfBgNVHSMEGDAWgBT0tuEgHf4prtLk
+# YaWyoiWyyBc1bjAdBgNVHQ4EFgQUNkSGjqS6sGa+vCgtHUQ23eNqerwwcQYDVR0f
+# BGowaDAyoDCgLoYsaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL3NoYTItYXNzdXJl
+# ZC10cy5jcmwwMqAwoC6GLGh0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9zaGEyLWFz
+# c3VyZWQtdHMuY3JsMIGFBggrBgEFBQcBAQR5MHcwJAYIKwYBBQUHMAGGGGh0dHA6
+# Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBPBggrBgEFBQcwAoZDaHR0cDovL2NhY2VydHMu
+# ZGlnaWNlcnQuY29tL0RpZ2lDZXJ0U0hBMkFzc3VyZWRJRFRpbWVzdGFtcGluZ0NB
+# LmNydDANBgkqhkiG9w0BAQsFAAOCAQEASBzctemaI7znGucgDo5nRv1CclF0CiNH
+# o6uS0iXEcFm+FKDlJ4GlTRQVGQd58NEEw4bZO73+RAJmTe1ppA/2uHDPYuj1UUp4
+# eTZ6J7fz51Kfk6ftQ55757TdQSKJ+4eiRgNO/PT+t2R3Y18jUmmDgvoaU+2QzI2h
+# F3MN9PNlOXBL85zWenvaDLw9MtAby/Vh/HUIAHa8gQ74wOFcz8QRcucbZEnYIpp1
+# FUL1LTI4gdr0YKK6tFL7XOBhJCVPst/JKahzQ1HavWPWH1ub9y4bTxMd90oNcX6X
+# t/Q/hOvB46NJofrOp79Wz7pZdmGJX36ntI5nePk2mOHLKNpbh6aKLzCCBTAwggQY
+# oAMCAQICEAQJGBtf1btmdVNDtW+VUAgwDQYJKoZIhvcNAQELBQAwZTELMAkGA1UE
+# BhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2lj
+# ZXJ0LmNvbTEkMCIGA1UEAxMbRGlnaUNlcnQgQXNzdXJlZCBJRCBSb290IENBMB4X
+# DTEzMTAyMjEyMDAwMFoXDTI4MTAyMjEyMDAwMFowcjELMAkGA1UEBhMCVVMxFTAT
+# BgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEx
+# MC8GA1UEAxMoRGlnaUNlcnQgU0hBMiBBc3N1cmVkIElEIENvZGUgU2lnbmluZyBD
+# QTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAPjTsxx/DhGvZ3cH0wsx
+# SRnP0PtFmbE620T1f+Wondsy13Hqdp0FLreP+pJDwKX5idQ3Gde2qvCchqXYJawO
+# eSg6funRZ9PG+yknx9N7I5TkkSOWkHeC+aGEI2YSVDNQdLEoJrskacLCUvIUZ4qJ
+# RdQtoaPpiCwgla4cSocI3wz14k1gGL6qxLKucDFmM3E+rHCiq85/6XzLkqHlOzEc
+# z+ryCuRXu0q16XTmK/5sy350OTYNkO/ktU6kqepqCquE86xnTrXE94zRICUj6whk
+# PlKWwfIPEvTFjg/BougsUfdzvL2FsWKDc0GCB+Q4i2pzINAPZHM8np+mM6n9Gd8l
+# k9ECAwEAAaOCAc0wggHJMBIGA1UdEwEB/wQIMAYBAf8CAQAwDgYDVR0PAQH/BAQD
+# AgGGMBMGA1UdJQQMMAoGCCsGAQUFBwMDMHkGCCsGAQUFBwEBBG0wazAkBggrBgEF
+# BQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tMEMGCCsGAQUFBzAChjdodHRw
+# Oi8vY2FjZXJ0cy5kaWdpY2VydC5jb20vRGlnaUNlcnRBc3N1cmVkSURSb290Q0Eu
+# Y3J0MIGBBgNVHR8EejB4MDqgOKA2hjRodHRwOi8vY3JsNC5kaWdpY2VydC5jb20v
+# RGlnaUNlcnRBc3N1cmVkSURSb290Q0EuY3JsMDqgOKA2hjRodHRwOi8vY3JsMy5k
+# aWdpY2VydC5jb20vRGlnaUNlcnRBc3N1cmVkSURSb290Q0EuY3JsME8GA1UdIARI
+# MEYwOAYKYIZIAYb9bAACBDAqMCgGCCsGAQUFBwIBFhxodHRwczovL3d3dy5kaWdp
+# Y2VydC5jb20vQ1BTMAoGCGCGSAGG/WwDMB0GA1UdDgQWBBRaxLl7KgqjpepxA8Bg
+# +S32ZXUOWDAfBgNVHSMEGDAWgBRF66Kv9JLLgjEtUYunpyGd823IDzANBgkqhkiG
+# 9w0BAQsFAAOCAQEAPuwNWiSz8yLRFcgsfCUpdqgdXRwtOhrE7zBh134LYP3DPQ/E
+# r4v97yrfIFU3sOH20ZJ1D1G0bqWOWuJeJIFOEKTuP3GOYw4TS63XX0R58zYUBor3
+# nEZOXP+QsRsHDpEV+7qvtVHCjSSuJMbHJyqhKSgaOnEoAjwukaPAJRHinBRHoXpo
+# aK+bp1wgXNlxsQyPu6j4xRJon89Ay0BEpRPw5mQMJQhCMrI2iiQC/i9yfhzXSUWW
+# 6Fkd6fp0ZGuy62ZD2rOwjNXpDd32ASDOmTFjPQgaGLOBm0/GkxAG/AeB+ova+YJJ
+# 92JuoVP6EpQYhS6SkepobEQysmah5xikmmRR7zCCBTEwggQZoAMCAQICEAqhJdbW
+# Mht+QeQF2jaXwhUwDQYJKoZIhvcNAQELBQAwZTELMAkGA1UEBhMCVVMxFTATBgNV
+# BAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEkMCIG
+# A1UEAxMbRGlnaUNlcnQgQXNzdXJlZCBJRCBSb290IENBMB4XDTE2MDEwNzEyMDAw
+# MFoXDTMxMDEwNzEyMDAwMFowcjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lD
+# ZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTExMC8GA1UEAxMoRGln
+# aUNlcnQgU0hBMiBBc3N1cmVkIElEIFRpbWVzdGFtcGluZyBDQTCCASIwDQYJKoZI
+# hvcNAQEBBQADggEPADCCAQoCggEBAL3QMu5LzY9/3am6gpnFOVQoV7YjSsQOB0Uz
+# URB90Pl9TWh+57ag9I2ziOSXv2MhkJi/E7xX08PhfgjWahQAOPcuHjvuzKb2Mln+
+# X2U/4Jvr40ZHBhpVfgsnfsCi9aDg3iI/Dv9+lfvzo7oiPhisEeTwmQNtO4V8CdPu
+# XciaC1TjqAlxa+DPIhAPdc9xck4Krd9AOly3UeGheRTGTSQjMF287DxgaqwvB8z9
+# 8OpH2YhQXv1mblZhJymJhFHmgudGUP2UKiyn5HU+upgPhH+fMRTWrdXyZMt7HgXQ
+# hBlyF/EXBu89zdZN7wZC/aJTKk+FHcQdPK/P2qwQ9d2srOlW/5MCAwEAAaOCAc4w
+# ggHKMB0GA1UdDgQWBBT0tuEgHf4prtLkYaWyoiWyyBc1bjAfBgNVHSMEGDAWgBRF
+# 66Kv9JLLgjEtUYunpyGd823IDzASBgNVHRMBAf8ECDAGAQH/AgEAMA4GA1UdDwEB
+# /wQEAwIBhjATBgNVHSUEDDAKBggrBgEFBQcDCDB5BggrBgEFBQcBAQRtMGswJAYI
+# KwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBDBggrBgEFBQcwAoY3
+# aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9v
+# dENBLmNydDCBgQYDVR0fBHoweDA6oDigNoY0aHR0cDovL2NybDQuZGlnaWNlcnQu
+# Y29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNybDA6oDigNoY0aHR0cDovL2Ny
+# bDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNybDBQBgNV
+# HSAESTBHMDgGCmCGSAGG/WwAAgQwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cu
+# ZGlnaWNlcnQuY29tL0NQUzALBglghkgBhv1sBwEwDQYJKoZIhvcNAQELBQADggEB
+# AHGVEulRh1Zpze/d2nyqY3qzeM8GN0CE70uEv8rPAwL9xafDDiBCLK938ysfDCFa
+# KrcFNB1qrpn4J6JmvwmqYN92pDqTD/iy0dh8GWLoXoIlHsS6HHssIeLWWywUNUME
+# aLLbdQLgcseY1jxk5R9IEBhfiThhTWJGJIdjjJFSLK8pieV4H9YLFKWA1xJHcLN1
+# 1ZOFk362kmf7U2GJqPVrlsD0WGkNfMgBsbkodbeZY4UijGHKeZR+WfyMD+NvtQEm
+# tmyl7odRIeRYYJu6DC0rbaLEfrvEJStHAgh8Sa4TtuF8QkIoxhhWz0E0tmZdtnR7
+# 9VYzIi8iNrJLokqV2PWmjlIwggVPMIIEN6ADAgECAhAE/eOq2921q55B9NnVIXVO
+# MA0GCSqGSIb3DQEBCwUAMHIxCzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2Vy
+# dCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20xMTAvBgNVBAMTKERpZ2lD
+# ZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25pbmcgQ0EwHhcNMjAwNzIwMDAw
+# MDAwWhcNMjMwNzI1MTIwMDAwWjCBizELMAkGA1UEBhMCR0IxEjAQBgNVBAcTCVdh
+# a2VmaWVsZDEmMCQGA1UEChMdU2VjdXJlIFBsYXRmb3JtIFNvbHV0aW9ucyBMdGQx
+# GDAWBgNVBAsTD1NjcmlwdGluZ0hlYXZlbjEmMCQGA1UEAxMdU2VjdXJlIFBsYXRm
+# b3JtIFNvbHV0aW9ucyBMdGQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
+# AQCvbSdd1oAAu9rTtdnKSlGWKPF8g+RNRAUDFCBdNbYbklzVhB8hiMh48LqhoP7d
+# lzZY3YmuxztuPlB7k2PhAccd/eOikvKDyNeXsSa3WaXLNSu3KChDVekEFee/vR29
+# mJuujp1eYrz8zfvDmkQCP/r34Bgzsg4XPYKtMitCO/CMQtI6Rnaj7P6Kp9rH1nVO
+# /zb7KD2IMedTFlaFqIReT0EVG/1ZizOpNdBMSG/x+ZQjZplfjyyjiYmE0a7tWnVM
+# Z4KKTUb3n1CTuwWHfK9G6CNjQghcFe4D4tFPTTKOSAx7xegN1oGgifnLdmtDtsJU
+# OOhOtyf9Kp8e+EQQyPVrV/TNAgMBAAGjggHFMIIBwTAfBgNVHSMEGDAWgBRaxLl7
+# KgqjpepxA8Bg+S32ZXUOWDAdBgNVHQ4EFgQUTXqi+WoiTm5fYlDLqiDQ4I+uyckw
+# DgYDVR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMHcGA1UdHwRwMG4w
+# NaAzoDGGL2h0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9zaGEyLWFzc3VyZWQtY3Mt
+# ZzEuY3JsMDWgM6Axhi9odHRwOi8vY3JsNC5kaWdpY2VydC5jb20vc2hhMi1hc3N1
+# cmVkLWNzLWcxLmNybDBMBgNVHSAERTBDMDcGCWCGSAGG/WwDATAqMCgGCCsGAQUF
+# BwIBFhxodHRwczovL3d3dy5kaWdpY2VydC5jb20vQ1BTMAgGBmeBDAEEATCBhAYI
+# KwYBBQUHAQEEeDB2MCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5j
+# b20wTgYIKwYBBQUHMAKGQmh0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdp
+# Q2VydFNIQTJBc3N1cmVkSURDb2RlU2lnbmluZ0NBLmNydDAMBgNVHRMBAf8EAjAA
+# MA0GCSqGSIb3DQEBCwUAA4IBAQBT3M71SlOQ8vwM2txshp/XDvfoKBYHkpFCyanW
+# aFdsYQJQIKk4LOVgUJJ6LAf0xPSN7dZpjFaoilQy8Ajyd0U9UOnlEX4gk2J+z5i4
+# sFxK/W2KU1j6R9rY5LbScWtsV+X1BtHihpzPywGGE5eth5Q5TixMdI9CN3eWnKGF
+# kY13cI69zZyyTnkkb+HaFHZ8r6binvOyzMr69+oRf0Bv/uBgyBKjrmGEUxJZy+00
+# 7fbmYDEclgnWT1cRROarzbxmZ8R7Iyor0WU3nKRgkxan+8rzDhzpZdtgIFdYvjeO
+# c/IpPi2mI6NY4jqDXwkx1TEIbjUdrCmEfjhAfMTU094L7VSNMYIEXDCCBFgCAQEw
+# gYYwcjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UE
+# CxMQd3d3LmRpZ2ljZXJ0LmNvbTExMC8GA1UEAxMoRGlnaUNlcnQgU0hBMiBBc3N1
+# cmVkIElEIENvZGUgU2lnbmluZyBDQQIQBP3jqtvdtaueQfTZ1SF1TjAJBgUrDgMC
+# GgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYK
+# KwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG
+# 9w0BCQQxFgQUC7eRsLOQhFOSLzssNQSWx+AKv2UwDQYJKoZIhvcNAQEBBQAEggEA
+# q6LSDx/oWEIj7Gm5pRBcUlYxa3TJ3u+ODafni6cuSCC3fIoRMtsc3TzM4WuLoKBL
+# DhS3rjsPV4/CgaUKwY12b9nHkZYvNGDC+U+pfL5l9S/79vLE2Bh1W906TC+Hio8W
+# wLhJgXS6L4fpGeVQBza9edGqo4pkX2MUe8Xwm/cLY/yQ7Ca1ezXli/xGOmxyIncm
+# vIGwevHcMAEFLxcWMOUARFw0ObcBOb8YvfUaZlFLTyWyTNi6bcJUO1lixft+ViCH
+# q0mp0qn8eQpNUGrPeY38CsVnN6j7SJ4ffXVs/Jj10+sVU/90Ya4xpXOiRT5+I4Dd
+# PmKcY6TYPLlZT4Ck8knvAqGCAjAwggIsBgkqhkiG9w0BCQYxggIdMIICGQIBATCB
+# hjByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQL
+# ExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3Vy
+# ZWQgSUQgVGltZXN0YW1waW5nIENBAhANQkrgvjqI/2BAIc4UAPDdMA0GCWCGSAFl
+# AwQCAQUAoGkwGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUx
+# DxcNMjIwMzA5MDkwMzIyWjAvBgkqhkiG9w0BCQQxIgQgrQdO6pgTtj65QAbyUjsg
+# aIKPIu7QvxOIiXF81u7CSKgwDQYJKoZIhvcNAQEBBQAEggEApG0zFEiPGIqID5I/
+# XAiTq9uN7YWVxl/LoaOM8qLVEhrSu1SU0XTq9TkXlJPiP/DCJsjDnlnvI/vObyVe
+# zjf6wtnwpB9kaXEfSZLw//4NsMF/DOKcrO3lwnU9tLwrCdqPnvAcLSkg7CN0XQfi
+# 5AkVeYaJgRiElf1DMmfdtr1Dk6Ym4voJFFoE3vBe1bSYLt5Oj/7qFugguNALxXNV
+# 8X9IP2IruoN20QBapyEw6lXf33BSgdv1eI7xq1iazb+2yNdiwkKCouR0wnvYya99
+# Qw/9e06N3BIvVkFbdY5KczSjcRlpcQPpRqITf5KNC2tRRvBfwKpxfcVRAa4shxZM
+# VnrOvw==
+# SIG # End signature block
